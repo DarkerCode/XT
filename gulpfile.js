de753cbb833015874d10e414a01ecf3f51c22d2e
xiaotongXT <xiaotong.wxt@qq.com>
2019-02-18 21:44:33 +0800

    two

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,23 @@
-const gulp=re=require("gulp");
+const gulp=require("gulp");
+const sass=require("gulp-sass");//需要安装gulp-sass npm install gulp-sass -D
+const path=require("path");
+
+
+const _path = {
+    src:path.join(__dirname,'src/**/*.scss'),
+    dist:path.join(__dirname,'dist'),
+    outputStyle:'expended'
+}
+
+
+gulp.task("styles",()=>{
+    return gulp.src(_path.src)
+    .pipe(sass({outputStyle:_path.outpathStyle}).on("error",sass.logError))
+    .pipe(gulp.dest(_path.dist))
+})
+
+gulp.task("watch",()=>{
+    return gulp.watch(_path.src,['styles'])
+})
+
+gulp.task("default",['styles','watch'])
